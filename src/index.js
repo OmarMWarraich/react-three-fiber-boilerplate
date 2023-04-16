2c22f7abc71d0199a3da7d64691a431da5b5aab0
OmarMWarraich <earthling986@gmail.com>
2023-04-16 11:45:49 +0500

    feat: Componentize

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Canvas } from "@react-three/fiber";
 import "./styles.css";
-
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Canvas camera={{ position: [0, 0, 2] }}>
-            <mesh>
-                <boxGeometry />
-                <meshBasicMaterial color="hotpink" wireframe/>
-            </mesh>
-        </Canvas>
+        <App />
     </StrictMode>
 );
